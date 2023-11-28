4626e4b3150c0bd499eccc8c87d780e055f177f0
shashanksb17 <sbasutkar999@gmail.com>
2023-11-28 23:28:39 +0530

    server updated

--- server.js
+++ server.js
@@ -8,28 +8,22 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Multer configuration
-const storage = multer.memoryStorage(); // Use memory storage for handling large files
+const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle file upload
 app.post('/upload', upload.single('file'), fileController.uploadFile);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
